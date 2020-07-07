977024007489f60dfe2786eeb131ccd89909d698
Jakub Malinowski <jakumal648@student.polsl.pl>
2020-07-07 21:27:20 +0200

    Added raw RecyclerView components

---
+++ app/src/main/java/com/remote/pum/organizer/NotesRecyclerViewAdapter.java
@@ -0,0 +1,39 @@
+package com.remote.pum.organizer;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class NotesRecyclerViewAdapter extends RecyclerView.Adapter<NotesRecyclerViewAdapter.ViewHolder> {
+    List<Note> notes;
+
+    public NotesRecyclerViewAdapter(List<Note> notes) {
+        this.notes = notes;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return notes.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
